018eb05af08a462b9bf2c2ba83e9ba543352d001
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-11-24 13:19:13 -0800

    Fix typo

--- gravity/pages/index.vue
+++ gravity/pages/index.vue
@@ -139,7 +139,7 @@ async function getIssues(token: string | null, after: string | null) {
 
 function computeLinks (nodeContainer: Array<Edge[]> | null)
 {
-  let relationships = any[];
+  let relationships:any = [];
 
   if (nodeContainer)
   {

38c6c839c8cc297d3f0be06ccb2b2d095da9186a
Den Delimarsky <1389609+dend@users.noreply.github.com>
2021-11-24 14:16:14 -0800

    Maybe this will work

--- gravity/components/IssueGraph.vue
+++ gravity/components/IssueGraph.vue
@@ -58,7 +58,7 @@ function createForceNetwork(nodes, edges) {
 
     //create a network from an edgelist
 
-    var force = d3.layout.force().nodes(nodes).links(edges)
+    var force = d3.force().nodes(nodes).links(edges)
         .size([500, 500])
         .charge(function(d) {
             return Math.min(-100, d.weight * -50)
